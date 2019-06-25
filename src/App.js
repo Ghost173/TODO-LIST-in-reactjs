e8bd30e2b3c9dfcd1dc44d2e7074be0306c701da
ghost <jpirakas007@gmail.com>
2019-06-25 10:30:15 +0530

    check boostrap and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import "bootstrap/dist/css/bootstrap.min.css"
+import uuid from 'uuid';
 
 function App() {
   return (
     <div>
-      hello from a[pp cmponets]
+      <div class="container">
+        <div class="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 text-capitalize text-center>Todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
