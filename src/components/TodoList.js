9f9cdc127b89416c3f8513b913faad98aeebe259
ghost <jpirakas007@gmail.com>
2019-06-27 10:26:47 +0530

    clear button with alignment

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
     render() {
         return (
             <div>
-                hello from to list
+               <ul className="list-group my-5">
+                   <h3 className="text-capitalize text-center"> todo list</h3>
+                   <TodoItem />
+                    <button className="btn btn-danger btn-block text-center mt-5"> clear list</button>
+               </ul>
             </div>
         )
     }
