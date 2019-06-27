58c2a2c6662f8ad497f95b229f2990a3ed9f06c8
ghost <jpirakas007@gmail.com>
2019-06-27 10:24:31 +0530

    import fotawesome icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'font-awesome/css/font-awesome.min.css';
 import uuid from 'uuid';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <div class="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 text-capitalize text-center>Todo input</h3>
+            
             <TodoInput />
             <TodoList />
           </div>
